4499d27da4ef3560c3cbf13bec06a5644305d6bb
Borja González Seoane <garaje@glezseoane.es>
2020-11-27 20:19:48 +0100

    Check input paths before call logic layer

--- finder_tags_butler/controller_layer.py
+++ finder_tags_butler/controller_layer.py
@@ -12,7 +12,7 @@
 
 """Finder Tags Butler core controller module."""
 
-from finder_tags_butler.cli_layer import run_parser
+from finder_tags_butler.cli_layer import run_parser, raise_error_printing
 from finder_tags_butler.logic_layer import *
 from finder_tags_butler.properties import MANIFEST_FILE_NAME
 
@@ -22,17 +22,29 @@ def main():
     opt, path = run_parser()
 
     # Calculate paths
-    cwd = os.getcwd()
-    cwd_manifest = os.path.join(cwd, MANIFEST_FILE_NAME)
+    if not os.path.isdir(path):
+        raise_error_printing(NotADirectoryError(path))
+    manifest_path = os.path.join(path, MANIFEST_FILE_NAME)
 
     # Interpret the option selected by the user
     if opt == "save_opt":
-        save_manifest(cwd, cwd_manifest)
-    elif opt == "dump_opt":
-        dump_manifest(manifest_path=cwd_manifest, path=cwd, force_overwriting=None)
-    elif opt == "soft_dump_opt":
-        dump_manifest(manifest_path=cwd_manifest, path=cwd, force_overwriting=False)
-    elif opt == "hard_dump_opt":
-        dump_manifest(manifest_path=cwd_manifest, path=cwd, force_overwriting=True)
-    else:  # If the parser is updated with this if-block, this won't occur
-        raise NotImplementedError
+        save_manifest(path, manifest_path)
+    else:
+        # Check manifest existence
+        if not os.path.isfile(manifest_path):
+            raise_error_printing(FileNotFoundError(manifest_path))
+
+        if opt == "dump_opt":
+            dump_manifest(
+                manifest_path=manifest_path, path=path, force_overwriting=None
+            )
+        elif opt == "soft_dump_opt":
+            dump_manifest(
+                manifest_path=manifest_path, path=path, force_overwriting=False
+            )
+        elif opt == "hard_dump_opt":
+            dump_manifest(
+                manifest_path=manifest_path, path=path, force_overwriting=True
+            )
+        else:  # If the parser is updated with this if-block, this won't occur
+            raise NotImplementedError
