1fcf0475d5465744d2673bc386513c4bd1e75b71
Borja González Seoane <garaje@glezseoane.es>
2020-11-28 00:14:02 +0100

    Manage controller logic tagging errors handling
    Rename function to do accord with its effect.

--- finder_tags_butler/controller_layer.py
+++ finder_tags_butler/controller_layer.py
@@ -32,39 +32,34 @@ def main():
     if opt == "save_opt":
         save_manifest(path=path, manifest_path=manifest_path)
         # If the process finish well...
-        order_ok_printing_without_exit(f"The manifest of {path} has been " f"saved. 💾")
+        order_ok_printing_and_exit(f"The manifest of {path} has been " f"saved. 💾")
     else:
         # Check manifest existence
         if not os.path.isfile(manifest_path):
             order_error_printing_and_exit(FileNotFoundError(manifest_path))
 
-        # Define the text to print on success
-        ok_msg_text = f"The manifest of {path} has been dumped. 🏷"
-
         if opt == "dump_opt":
-            dump_manifest(
+            tagging_errors = dump_manifest(
                 manifest_path=manifest_path, path=path, force_overwriting=None
             )
-            # If the process finish well...
-            order_ok_printing_without_exit(ok_msg_text)
-
         elif opt == "soft_dump_opt":
-            dump_manifest(
+            tagging_errors = dump_manifest(
                 manifest_path=manifest_path, path=path, force_overwriting=False
             )
-            # If the process finish well...
-            order_ok_printing_without_exit(ok_msg_text)
         elif opt == "hard_dump_opt":
-            dump_manifest(
+            tagging_errors = dump_manifest(
                 manifest_path=manifest_path, path=path, force_overwriting=True
             )
-            # If the process finish well...
-            order_ok_printing_without_exit(ok_msg_text)
         else:  # If the parser is updated with this if-block, this won't occur
             raise NotImplementedError
 
+        for tag_error in tagging_errors:
+            order_error_printing_without_exit(tag_error)
+        # If the process finish well...
+        order_ok_printing_and_exit(f"The manifest of {path} has been dumped. 🏷")
 
-def order_ok_printing_without_exit(msg_text: str) -> None:
+
+def order_ok_printing_and_exit(msg_text: str) -> None:
     """:param msg_text: The message text to print."""
     print_ok(msg_text)
     sys.exit(0)
