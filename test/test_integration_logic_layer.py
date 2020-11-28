e36b25303178fcc7612946e7ddc83951cd96a824
Borja González Seoane <garaje@glezseoane.es>
2020-11-28 01:01:31 +0100

    Write a test to manifest validations

--- test/test_integration_logic_layer.py
+++ test/test_integration_logic_layer.py
@@ -14,6 +14,7 @@
 
 import os
 import random
+import shutil
 import socket
 import string
 import tempfile
@@ -26,6 +27,7 @@ from finder_tags_butler.logic_layer import (
     _get_children_of_path,
     save_manifest,
     dump_manifest,
+    _validate_mainifest_file,
 )
 from finder_tags_butler.logic_tags import (
     add_finder_tag_for_path,
@@ -94,6 +96,68 @@ class IntegrationTestSuiteLogicLayer(TestCase):
                     # Achieve an empty list
                     res_tags.remove(tags[i])
                     self.assertFalse(res_tags)
+
+    def test_manifest_files_validation(self):
+        with tempfile.TemporaryDirectory() as sample_node:
+            # Generate random stuff
+            _generate_random_folders_tree(sample_node)
+
+            # Select some random files and folders
+            children = _get_children_of_path(sample_node)
+            tagged_children = children[: (len(children) // 3)]  # Children subset
+
+            # Create a list with a random tag name to each tagged child
+            tags = []
+            for _ in tagged_children:
+                tags.append(
+                    "".join(random.choice(string.ascii_letters) for i in range(15))
+                )  # Random tag name
+
+            # Tag children
+            additional_tag_1 = "Multiple of eleven"
+            additional_tag_2 = "Multiple of 11"
+            for i in range(0, len(tagged_children)):
+                add_finder_tag_for_path(tagged_children[i], tags[i])
+                if not i % 11:
+                    add_finder_tag_for_path(tagged_children[i], additional_tag_1)
+                    add_finder_tag_for_path(tagged_children[i], additional_tag_2)
+
+            # Save the manifest
+            manifest_path = os.path.join(sample_node, MANIFEST_FILE_NAME)
+            save_manifest(sample_node, manifest_path)
+
+            # Save several copies of the manifest
+            manifest_path_copy1 = f"{manifest_path}.copy1"
+            manifest_path_copy2 = f"{manifest_path}.copy2"
+            manifest_path_copy3 = f"{manifest_path}.copy3"
+            shutil.copy(manifest_path, manifest_path_copy1)
+            shutil.copy(manifest_path, manifest_path_copy2)
+            shutil.copy(manifest_path, manifest_path_copy3)
+
+            # Check validity of initial manifest
+            self.assertTrue(_validate_mainifest_file(manifest_path))
+
+            # Insert random dirty text at the head of the file
+            with open(manifest_path_copy1, "w") as f:
+                f.seek(0)
+                f.write("fñv´´zñvsfvlausvbsvs<dfsdagfasb<cygidsvysGCV<UDS")
+
+            self.assertFalse(_validate_mainifest_file(manifest_path_copy1))
+
+            # Insert incomplete manifest representation
+            with open(manifest_path_copy2, "w") as f:
+                yaml.dump({"machine": "computer"}, f)
+
+            self.assertFalse(_validate_mainifest_file(manifest_path_copy2))
+
+            # Change name of component
+            with open(manifest_path_copy3, "r") as f:
+                filedata = f.read()
+            filedata = filedata.replace("content", "wine")
+            with open(manifest_path_copy3, "w") as f:
+                f.write(filedata)
+
+            self.assertFalse(_validate_mainifest_file(manifest_path_copy3))
 
 
 if __name__ == "__main__":
